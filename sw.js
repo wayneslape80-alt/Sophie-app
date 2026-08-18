703647ba0f338f62ecf3aaeb9695049fddfca918
wayneslape80-alt <wayneslape80@gmail.com>
2026-08-18 17:22:54 +0930

    Modify service worker cache and assets
    Updated cache name and added new icon files.

--- sw.js
+++ sw.js
@@ -1,5 +1,13 @@
-const CACHE_NAME = "sophie-app-v2-1";
-const APP_FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
+const CACHE_NAME = "sophie-app-v2-2";
+
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon.svg",
+  "./icon-192.png",
+  "./icon-512.png"
+];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES)));
